81e01da3c5ca2de809777f661c1a11c274f34797
Aarya Shetty <aaryashetty100@gmail.com>
2025-10-24 17:36:03 +0530

    Update fetch URL to live backend

--- Frontend/src/Main.purs
+++ Frontend/src/Main.purs
@@ -153,7 +153,7 @@ handleAction = case _ of
 
   FetchQuotes -> do
     H.modify_ _ { loading = true, error = Nothing }
-    response <- H.liftAff $ get ResponseFormat.json "http://localhost:8080/api/quotes"
+    response <- H.liftAff $ get ResponseFormat.json "https://random-quote-explorer-using-purescript.onrender.com"
     handleAction $ ReceiveQuotes response
 
   ReceiveQuotes response -> case response of
